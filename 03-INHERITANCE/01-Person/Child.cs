560d79f1a13fac655c256ec22e07631290dba58d
VladoG70 <shopping@radiovot.net>
2017-07-10 14:29:34 +0300

    01-Person
    Inheritance - Exercise

---
+++ 03-INHERITANCE/01-Person/Child.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Person
+    {
+    public class Child : Person
+        {
+        public Child(string name, int age) : base(name, age)
+            { }
+
+        public override int Age
+            {
+            get { return base.Age; }
+            set
+                {
+                if (value > 15)
+                    {
+                    throw new ArgumentException(@"Child's age must be less than 15!");
+                    }
+                base.Age = value;
+                }
+            }
+
+        public override string ToString()
+            {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                this.Name,
+                this.Age));
+
+            return stringBuilder.ToString();
+            }
+        }
+    }
